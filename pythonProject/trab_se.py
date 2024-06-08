95b432d78ae75bacd049a11d5f4dc9635c0d3625
Gabriel Mariano <gabrielmarianogsantos@gmail.com>
2024-06-08 13:26:44 -0300

    commit ligar o led

--- pythonProject/trab_se.py
+++ pythonProject/trab_se.py
@@ -1,64 +1,38 @@
-# from flask import Flask, request, jsonify
-
-# app = Flask(__name__)
-# app.config['JSON_AS_ASCII'] = False
-
-# @app.route('/hello')
-# def say_hello():
-#     return "Hello world!"
-# '''
-# @app.route("/", methods=["Get", "Post"])
-# def form():
-#     if request.method == "Post":
-#         name = request.form["name"]
-#         email = request.form["email"]
-#         return 
-        
-#         <!DOCTYPE html>
-#         <html>
-#         <head>
-#             <title>Simple Form</title>
-#         </head>
-#         <body>
-#             <h1>Simple Form</h1>
-#             <form method="POST" action="/">
-#                 <label for="name">Name:</label>
-#                 <input type="text"  id="name" name="name">
-#             </form>
-#         </body>
-#         </html>
-# '''
-# if __name__ == "__main__":
-#     app.run(debug=True)
-from flask import Flask, request, jsonify
-
+from flask import Flask,render_template, redirect, request, url_for
+from gpiozero import LED
+from time import sleep
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 @app.route('/bbb')
 def say_hello():
     return "Hello world!"
-'''
-@app.route("/", methods=["Get", "Post"])
-def form():
-    if request.method == "Post":
-        name = request.form["name"]
-        email = request.form["email"]
-        return 
-        
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Simple Form</title>
-        </head>
-        <body>
-            <h1>Simple Form</h1>
-            <form method="POST" action="/">
-                <label for="name">Name:</label>
-                <input type="text"  id="name" name="name">
-            </form>
-        </body>
-        </html>
-'''
+
+@app.route('/bbb1')
+def teste():
+    led = LED(17)
+
+    while True:
+        led.on()
+        sleep(1)
+        led.off()
+        sleep(1)
+
+@app.route('/ledon', methods=["POST", "GET"])
+def led_on():
+    
+    if request.method == "POST":
+        valor = request.form["nm"]
+        if valor == "ON":
+            return redirect(url_for("teste"))
+        elif valor == "OFF":
+            return redirect(url_for("teste"))
+    else:
+        return render_template("ligabutao.html")
+    
+@app.route('/led')
+def led():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(debug=True, port = 5001, host="0.0.0.0")
