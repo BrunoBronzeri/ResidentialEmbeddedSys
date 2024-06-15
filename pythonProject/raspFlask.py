9dfdfe99a66bdd37eda443cbd052b063b2c4367f
Bruno Bueno Bronzeri <brunobronzeri@icloud.com>
2024-06-15 12:43:50 -0300

    testFlaskRasp

---
+++ pythonProject/raspFlask.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, redirect, request, url_for, Response
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+@app.route('/hello')
+def say_hello():
+    return "Hello world!"
+
+if __name__ == "__main__":
+    app.run(debug=True, port = 5001, host="0.0.0.0")
+    
